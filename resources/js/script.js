288ef92fb2247ac2f2b99ef2d448bb1d23bf5669
Zobair Qauomi <zobairq55@gmail.com>
2020-10-12 19:21:20 +0200

    Finalizing the responsive menu.

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,7 +24,48 @@ $(document).ready(() => {
       1000
     );
   });
+
+  $(".js--wp-1").waypoint(
+    function () {
+      $(".js--wp-1").addClass("animate__animated animate__fadeInRight");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-2").waypoint(
+    function () {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-3").waypoint(
+    function () {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--wp-4").waypoint(
+    function () {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    { offset: "50%" }
+  );
+
+  $(".js--mobile-nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--mobile-nav-icon ion-icon");
+
+    nav.slideToggle(200);
+    if (icon.attr("name") === "menu-outline") {
+      icon.attr("name", "close-outline");
+    } else {
+      icon.attr("name", "menu-outline");
+    }
+  });
 });
+
 $(function () {
   $("a[href*=\\#]:not([href=\\#])").click(function () {
     if (
